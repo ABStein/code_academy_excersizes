cb1c92903bb0fc972cd4d97293f52cb8de59ff0b
Andrew Stein <ABStein@github.com>
2019-01-27 19:26:10 -0600

    more fun with conditionals with not statements

--- conditionals.py
+++ conditionals.py
@@ -11,3 +11,11 @@ def graduation_reqs(credits):
     return "You have enough credits to graduate!"
   
 print(graduation_reqs(120))
+
+
+def applicant_selector(gpa, ps_score, ec_count):
+  if (gpa >= 3.0) and (ps_score >= 90) and (ec_count >= 3):
+    return "This applicant should be accepted."
+  elif (gpa >= 3.0) and (ps_score >= 90) and not (ec_count >= 3):
+    return "This applicant should be given an in-person interview."
+  else: return "This applicant should be rejected."
